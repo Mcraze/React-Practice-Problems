ee8adfa047bf475d369a0269fd984f5091ca7ff7
Mitesh Adelkar <adelkarmitesh23@gmail.com>
2025-10-31 19:01:33 +0530

    Added Difficulty Filter

--- src/site_specific/MainSection.jsx
+++ src/site_specific/MainSection.jsx
@@ -5,19 +5,30 @@ import { useState } from "react";
 const MainSection = () => {
 
     const [searchProblem, setSearchProblem] = useState("");
+    const [difficultyFilter, setDifficultyFilter] = useState("");
 
-    const filteredProblems = problemsList.filter((problem) =>
-        problem.name.toLowerCase().includes(searchProblem.toLowerCase())
-    );
+    const filteredProblems = problemsList.filter((problem) => {
+        const problemName = problem.name.toLowerCase().includes(searchProblem.toLowerCase())
+        const problemDifficulty = problem.difficulty.includes(difficultyFilter)
+        return problemName && problemDifficulty;
+    });
 
     return (
         <div className="min-h-screen">
             <div className="text-center rounded-lg bg-emerald-400 flex justify-center items-center px-4 py-20 md:py-25 lg:py-30">
                 <h1 className="text-3xl sm:text-4xl md:text-6xl lg:text-7xl text-white">REACT PROBLEMS</h1>
             </div>
-            <div className="mt-12 mb-6 grid gap-4 sm:grid-cols-2 items-center">
+            <div className="mt-12 mb-6 grid gap-4 md:grid-cols-2 items-center">
                 <h2 className="text-2xl md:text-4xl">Problem List</h2>
-                <input type="text" placeholder="Search problems..." onChange={(e) => setSearchProblem(e.target.value)} />
+                <div className="grid sm:grid-cols-2 gap-2">
+                    <input type="text" placeholder="Search problems..." onChange={(e) => setSearchProblem(e.target.value)} />
+                    <select onChange={e => setDifficultyFilter(e.target.value)}>
+                        <option value="">All Difficulties</option>
+                        <option value="Easy">Easy</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Hard">Hard</option>
+                    </select>
+                </div>
             </div>
             <div className="grid md:grid-cols-3 gap-6">
                 {filteredProblems.length !== 0 ? filteredProblems.map((problem) => (
